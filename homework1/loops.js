10cef90532910f5821c975985d5c7f4bc695263c
Nar <nt3244@gmail.com>
2022-07-24 20:36:15 -0400

    finished loop practice

---
+++ homework1/loops.js
@@ -0,0 +1,21 @@
+for (let a = 0; a < 100; a++) {
+    console.log(a);
+}
+for (let b = 99; b >= 0; b--) {
+    console.log(b);
+}
+for (let c = 0; c <= 98; c++) {
+    if (c % 2 == 0) {
+        console.log(c);
+    }
+}
+for (let d = 99; d >= 0; d--) {
+    if (d % 2 != 0) {
+        console.log(d);
+    }
+}
+for (let e = 3; e <= 90; e++) {
+    if (e % 3 == 0) {
+        console.log(e);
+    }
+}
